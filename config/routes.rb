8cb0b8fd2ab32428c72941c2d4b621d6f60e7c8d
oleksandr <olexandr7@gmail.com>
2017-12-25 03:09:43 +0200

    Map displayed correctly

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'view/index'
+  root 'view#index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
